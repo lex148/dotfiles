74c3a93dca222bcfea74454c0c0b6d670fd0ff3f
lex148 <lexchilds@gmail.com>
2017-12-18 11:06:25 -0600

    general collection of useful dotfiles

---
+++ config.fish
@@ -0,0 +1,11 @@
+set PATH /opt/android-sdk/platform-tools $PATH
+set PATH /opt/android-sdk/tools $PATH
+
+set PATH /usr/local/heroku/bin $PATH
+set PATH /home/lex/.gem/ruby/2.1.0/bin $PATH
+#alias screen '~/Sync/dot_files/screen'
+#alias tmx '~/Sync/dot_files/tmx'
+alias rails 'bundle exec rails'
+alias rake 'bundle exec rake'
+
+
